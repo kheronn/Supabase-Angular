2b8b31cd77b1983d482f0168f43d02dfeb86cd5a
unknown <kheronn@gmail.com>
2021-03-04 19:29:25 -0300

    Update polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -59,6 +59,10 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+(window as any).process = {
+    env: { DEBUG: undefined },
+  };
+  
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
