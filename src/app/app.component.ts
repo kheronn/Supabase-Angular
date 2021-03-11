64b21cd1616ab4886a3078e0d8e6386cd1870263
unknown <kheronn@gmail.com>
2021-03-11 14:59:37 -0300

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,46 +5,47 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
   todos: Todo[];
   todo: Todo;
-  actionLabel: string = "ADD"
+  actionLabel: string;
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService) {}
 
   async ngOnInit(): Promise<void> {
     let listen = this.api.listenAll();
-    this.api.getTodos()
-      .then(data => this.todos = data.todos)
-    this.todo = new Todo();
+    this.api.getTodos().then((data) => (this.todos = data.todos));
+    this.clear();
   }
 
   addTodo() {
-    if (this.todo.id) { //Update if exists ID{
+    if (this.todo.id) {
+      //Update if exists ID{
       this.update();
       return;
     }
-    this.api.addTodo(this.todo).then((payload) => {
-      this.todos.push(payload.data[0]);
-    }).catch(erro => console.log(`Error in add TODO ${erro}`))
+    this.api
+      .addTodo(this.todo)
+      .then((payload) => {
+        this.todos.push(payload.data[0]);
+      })
+      .catch((erro) => console.log(`Error in add TODO ${erro}`));
     this.clear();
   }
 
   seleciona(todo: Todo) {
     this.todo = todo;
-    this.actionLabel = "UPDATE";
+    this.actionLabel = 'UPDATE';
   }
 
   update() {
-    this.api.update(this.todo)
-      .then(() => {
-        let foundIndex = this.todos.findIndex(t => t.id == this.todo.id);
-        this.todos[foundIndex] = this.todo;
-        this.clear();
-      })
+    this.api.update(this.todo).then(() => {
+      let foundIndex = this.todos.findIndex((t) => t.id == this.todo.id);
+      this.todos[foundIndex] = this.todo;
+      this.clear();
+    });
   }
 
   check(todoCheck: Todo) {
@@ -54,22 +55,17 @@ export class AppComponent implements OnInit {
   }
 
   delete(todo: Todo) {
-    this.api.deleteTodo(todo.id)
-      .then(dados => {
-        console.log(dados)
-        this.todos = this.arrayRemove(this.todos, todo.id);
-      })
+    this.api
+      .deleteTodo(todo.id)
+      .then((dados) => (this.todos = this.arrayRemove(this.todos, todo.id)));
   }
 
-  arrayRemove(arr, value) {
-    return arr.filter((ele) => {
-      return ele.id != value;
-    });
+  arrayRemove(arr: Todo[], id: string) {
+    return arr.filter((ele) => ele.id != id);
   }
 
   clear() {
     this.todo = new Todo();
-    this.actionLabel = "ADD";
+    this.actionLabel = 'ADD';
   }
-
 }
