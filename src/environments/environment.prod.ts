f5261585651fe0eb3de2aaa8669b91ce99cfa908
Kheronn <kheronn@gmail.com>
2021-07-29 09:59:02 -0300

    Update: @supabase/supabase-js

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,4 @@ export const environment = {
   production: true,
   supabaseUrl: "https://eryktzqdtxyesygmuxeg.supabase.co",
   supbaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNDU1Mjg4MywiZXhwIjoxOTMwMTI4ODgzfQ.tr25Nyviyvj3vcmZMPbBqRCnt6XcOc4Ui6the_WWh9s"
-
 };
